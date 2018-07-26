0285857bdbe74c8956444701c1415c30668d02d6
Mathieu Champlon <mathieu.champlon@docker.com>
2018-07-26 19:49:33 +0200

    Condense RUN in docker build

--- dep.go
+++ dep.go
@@ -9,9 +9,8 @@ func Dep(args ...string) Tool {
 		"https://github.com/golang/dep",
 		`
 FROM alpine:`+AlpineVersion+`
-RUN apk add --no-cache curl
-
-RUN curl -o /usr/bin/dep -L https://github.com/golang/dep/releases/download/`+DepVersion+`/dep-linux-amd64 && \
+RUN apk add --no-cache curl && \
+    curl -o /usr/bin/dep -L https://github.com/golang/dep/releases/download/`+DepVersion+`/dep-linux-amd64 && \
     chmod +x /usr/bin/dep
 `)
 	if len(args) > 0 {
