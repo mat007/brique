eb2854e52cf13807799172341ae6c824da8e05f6
Mathieu Champlon <mathieu.champlon@docker.com>
2018-07-26 08:17:41 +0200

    Add more git helpers

--- git.go
+++ git.go
@@ -28,14 +28,31 @@ RUN apk add --no-cache git
 	return gitTool
 }
 
-func GitCommit() string {
+func GitShortCommit() string {
 	buf := &bytes.Buffer{}
 	Git().WithOutput(buf).WithSuccess().Run("rev-parse", "--short", "HEAD")
+	return strings.TrimSuffix(buf.String(), "\n")
+}
+
+func GitCommit() string {
+	buf := &bytes.Buffer{}
+	Git().WithOutput(buf).WithSuccess().Run("rev-parse", "HEAD")
 	return strings.TrimSuffix(buf.String(), "\n")
 }
 
 func GitTag() string {
 	buf := &bytes.Buffer{}
 	Git().WithOutput(buf).WithSuccess().Run("describe", "--always", "--dirty")
+	return strings.TrimSuffix(buf.String(), "\n")
+}
+
+func GitDirty() bool {
+	Git("update-index", "-q", "--refresh")
+	return Git().WithSuccess().Run("diff-index", "--quiet", "HEAD", "--", ".")
+}
+
+func GitVersion() string {
+	buf := &bytes.Buffer{}
+	Git().WithOutput(buf).WithSuccess().Run("tag", "-l", "--points-at", "HEAD", `"v*"`)
 	return strings.TrimSuffix(buf.String(), "\n")
 }
