6b6a50ad7b503736567ab0a81cb986298674db9b
Mathieu Champlon <mathieu.champlon@docker.com>
2018-07-27 14:07:12 +0200

    Simplify image build

--- gometalinter.go
+++ gometalinter.go
@@ -7,16 +7,13 @@ func GoMetaLinter(args ...string) Tool {
 		"gometalinter",
 		"--version",
 		"https://github.com/alecthomas/gometalinter",
-		`
-FROM golang:`+GoVersion+`-alpine`+AlpineVersion+`
-RUN apk add --no-cache curl
-
+		`FROM golang:`+GoVersion+`-alpine`+AlpineVersion+`
 WORKDIR /go/src/github.com/alecthomas/gometalinter
-RUN curl -L https://github.com/alecthomas/gometalinter/archive/v`+GoMetaLinterVersion+`.tar.gz | tar xz --strip-components=1 && \
+RUN apk add --no-cache curl && \
+    curl -L https://github.com/alecthomas/gometalinter/archive/v`+GoMetaLinterVersion+`.tar.gz | tar xz --strip-components=1 && \
 	go build -v -o /usr/local/bin/gometalinter . && \
 	gometalinter --install && \
-	rm -rf /go/src/* /go/pkg/*
-`)
+	rm -rf /go/src/* /go/pkg/*`)
 	if len(args) > 0 {
 		t.Run(args...)
 	}
