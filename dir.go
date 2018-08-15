177bf8c5a2747229500aa6e7d9cf404e23e24274
Mathieu Champlon <mathieu.champlon@docker.com>
2018-08-15 20:18:02 +0200

    Move Dir to B

--- dir.go
+++ dir.go
@@ -5,20 +5,21 @@ import (
 	"os"
 )
 
-func Dir(dir string, f func()) {
-	current, err := os.Getwd()
+func (b *B) Dir(dir string, f func()) {
+	wd, err := os.Getwd()
 	if err != nil {
 		Fatalln("cannot get current directory:", err)
 	}
 	// $$$$ MAT: this fails if inside a parallel section...
 	defer func() {
-		if err = os.Chdir(current); err != nil {
+		if err = os.Chdir(wd); err != nil {
 			Fatalln("cannot change back current directory:", err)
 		}
 	}()
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		Fatalln("cannot create directory:", err)
 	}
+	// $$$$ MAT: does not work in container build because the working dir gets mounted as root
 	if err := os.Chdir(dir); err != nil {
 		Fatalln("cannot change current directory:", err)
 	}
