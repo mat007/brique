488af984d0ccaff9ea06353de7c279d8f269c4fb
Mathieu Champlon <mathieu.champlon@docker.com>
2018-08-23 16:58:08 +0200

    Factorize build tool location

--- build.bat
+++ build.bat
@@ -2,6 +2,8 @@
 
 rem set the project package name
 set PACKAGE_NAME=github.com/mat007/b
+rem in a user project this would point to the vendored folder
+set BUILD_TOOL=./cmd/b
 
 rem configure current platform
 set MSYS_NO_PATHCONV=1
@@ -11,7 +13,7 @@ rem test if go is available
 go version >nul 2>nul
 if errorlevel 1 goto docker
 rem build the build tool directly
-go build ./cmd/b && b.exe %*
+go build %BUILD_TOOL% && b.exe %*
 goto end
 
 :docker
@@ -20,7 +22,7 @@ docker version >nul 2>nul
 if errorlevel 1 goto error
 rem build the build tool in a container
 docker run --rm -t -v%cd%:/go/src/%PACKAGE_NAME% -e GOOS=%GOOS% ^
-    -w /go/src/%PACKAGE_NAME% golang:1.10.3-alpine3.7 go build ./cmd/b ^
+    -w /go/src/%PACKAGE_NAME% golang:1.10.3-alpine3.7 go build %BUILD_TOOL% ^
     && b.exe %*
 goto end
 
--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 # set the project package name
 PACKAGE_NAME=github.com/mat007/b
+# in a user project this would point to the vendored folder
+BUILD_TOOL=./cmd/b
 
 # configure current platform
 if [ "${OS}" = "Windows_NT" ]; then
@@ -14,13 +16,13 @@ fi
 # test if go is available
 if [ -x "$(command -v go)" ]; then
     # build the build tool directly
-    go build ./cmd/b
+    go build $BUILD_TOOL
 else
     # test if docker is available
     if [ -x "$(command -v docker)" ]; then
         # build the build tool in a container
         docker run --rm -t -v"$(pwd)":/go/src/$PACKAGE_NAME -e GOOS=$GOOS \
-            -w /go/src/$PACKAGE_NAME golang:1.10.3-alpine3.7 go build ./cmd/b
+            -w /go/src/$PACKAGE_NAME golang:1.10.3-alpine3.7 go build $BUILD_TOOL
     else
         echo "Either go (http://golang.org) or docker (http://www.docker.com) needed to build."
         exit 1
